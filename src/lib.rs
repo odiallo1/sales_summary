15808682514d50abfc1d3b1a49f7633b2b16ae3f
odiallo1 <odiallo1@my.westga.edu>
2023-10-14 21:06:56 -0400

    added lib and finished version 1

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+use std::fs::OpenOptions;
+use std::io::{self, BufRead, BufReader, Write};
+
+pub fn process_input_file(folder_path: &str) -> Result<String, io::Error> {
+    let input_file_path = format!("{}/branch_weekly_sales.txt", folder_path);
+    let file = match std::fs::File::open(&input_file_path) {
+        Ok(file) => file,
+        Err(e) => return Err(io::Error::new(io::ErrorKind::Other, format!("Failed to open file: {}", e))),
+    };
+    let reader = BufReader::new(file);
+
+    let mut total_sales = 0;
+    for line in reader.lines() {
+        let line = match line {
+            Ok(line) => line,
+            Err(e) => return Err(io::Error::new(io::ErrorKind::Other, format!("Failed to read line: {}", e))),
+        };
+        let parts: Vec<&str> = line.split(", ").collect();
+        if let Ok(sales) = parts[2].parse::<i32>() {
+            total_sales += sales;
+        } else {
+            return Err(io::Error::new(io::ErrorKind::Other, "Failed to parse sales data"));
+        }
+    }
+    
+    let output_str = format!("{}, PROD001, {}", folder_path, total_sales);
+    write_to_summary_file(&output_str)?;
+    Ok("OK".to_string())
+}
+
+pub fn write_to_summary_file(output_str: &str) -> Result<(), io::Error> {
+    let output_file_path = "/sales_summary/data/weekly_summary/weekly_sales_summary.txt";
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(output_file_path)?;
+    writeln!(file, "{}", output_str)?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+mod lib;
+
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} /sales_summary/data", args[0]);
+        process::exit(1);
+    }
+    let data_folder_path = &args[1];
+
+    let output_dir = format!("{}/weekly_summary", data_folder_path);
+    if !fs::metadata(&output_dir).is_ok() {
+        fs::create_dir_all(&output_dir).expect("Failed to create output directory");
+    }
+
+    let start = std::time::Instant::now();
+
+    for i in 1..=40 {
+        let folder_path = format!("{}/branch{}", data_folder_path, i);
+        match lib::process_input_file(&folder_path) {
+            Ok(_) => println!("Processed branch{}", i),
+            Err(e) => eprintln!("Failed to process branch{}: {}", i, e),
+        }
+    }
+
+    let duration = start.elapsed();
+    println!("Processing time: {:?}", duration);
+
+    println!("Phew! I am done.");
 }
