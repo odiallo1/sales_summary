3e0df06af5c628ebb58ba467388ed88aaefbac3b
odiallo1 <odiallo1@my.westga.edu>
2023-10-15 13:50:38 -0400

    fixed input file path

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,22 @@ use std::fs;
 use std::time::Instant;
 
 fn main() {
-    let output_dir = "sales_summary/data/weekly_summary";
+    let branches = vec![
+        "ALBNM",
+        "CTONGA", 
+    ];
+    
+    let output_dir = "data/data/weekly_summary";
     if !fs::metadata(&output_dir).is_ok() {
         fs::create_dir_all(&output_dir).expect("Failed to create output directory");
     }
 
     let start = Instant::now();
 
-    for i in 1..=40 {
-        match lib::process_input_file(i) {
-            Ok(_) => println!("Processed branch{}", i),
-            Err(e) => eprintln!("Failed to process branch{}: {}", i, e),
+    for branch in &branches {
+        match lib::process_input_file(branch) {
+            Ok(_) => println!("Processed {}", branch),
+            Err(e) => eprintln!("Failed to process {}: {}", branch, e),
         }
     }
 
