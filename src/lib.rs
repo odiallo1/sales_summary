6014330c7908a910c8ccbfd1518fa0ea323fb6de
odiallo1 <odiallo1@my.westga.edu>
2023-11-07 23:24:42 -0500

    finished version2 and added branch folders

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,29 @@
-use std::fs::OpenOptions;
+use std::fs::File;
 use std::io::{self, BufRead, BufReader, Write};
+use std::fs::OpenOptions;
 
 pub fn process_input_file(branch: &str) -> Result<String, io::Error> {
     let input_file_path = format!("data/data/{}/branch_weekly_sales.txt", branch);
-    let file = match std::fs::File::open(&input_file_path) {
-        Ok(file) => file,
-        Err(e) => return Err(io::Error::new(io::ErrorKind::Other, format!("Failed to open file: {}", e))),
-    };
+    let file = File::open(&input_file_path)?;
     let reader = BufReader::new(file);
 
     let mut total_sales = 0;
-    for line in reader.lines() {
-        let line = match line {
-            Ok(line) => line,
-            Err(e) => return Err(io::Error::new(io::ErrorKind::Other, format!("Failed to read line: {}", e))),
-        };
+    let mut product_code = String::new();
+    for (index, line) in reader.lines().enumerate() {
+        let line = line?;
         let parts: Vec<&str> = line.split(", ").collect();
-        if let Ok(sales) = parts[2].parse::<i32>() {
-            total_sales += sales;
-        } else {
-            return Err(io::Error::new(io::ErrorKind::Other, "Failed to parse sales data"));
+        if index == 0 {
+            product_code = parts[1].to_owned();
         }
+        total_sales += parts[2].parse::<i32>().map_err(|_| io::Error::new(io::ErrorKind::Other, "Failed to parse sales data"))?;
     }
-    
-    let output_str = format!("{}, PROD001, {}", branch, total_sales);
+
+    let output_str = format!("{}, {}, {}", branch, product_code, total_sales);
     write_to_summary_file(&output_str)?;
     Ok("OK".to_string())
 }
 
-pub fn write_to_summary_file(output_str: &str) -> Result<(), io::Error> {
+fn write_to_summary_file(output_str: &str) -> Result<(), io::Error> {
     let output_file_path = "data/data/weekly_summary/weekly_sales_summary.txt";
     let mut file = OpenOptions::new()
         .write(true)
